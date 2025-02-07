448bd67bc8e4c06a9e6141b803de4f1cfd822537
Devansh <92976625+Dash198@users.noreply.github.com>
2025-02-07 09:38:36 +0530

    Delete Ocaml/sdf directory

--- Ocaml/sdf/hello.ml
+++
@@ -1,9 +0,0 @@
-open Stdio
-
-let hello_world name = printf "Hello %s\n" (name)
-
-let _ = 
-  let line = In_channel.input_line In_channel.stdin in
-  match line with
-  |None -> hello_world "Anon"
-  |Some x -> hello_world x
